004af3d2bcc281eb05c3bfb4db93815018e69ee5
AkiPoh <59505647+AkiPoh@users.noreply.github.com>
2025-12-27 10:25:07 +0200

    Make pass 2 modern style

--- akidocs-core/src/akidocs_core/styles.py
+++ akidocs-core/src/akidocs_core/styles.py
@@ -1,6 +1,6 @@
 from akidocs_core.style_base import Style, pt_to_mm
 
-pt = pt_to_mm  # Alias for brevity
+pt = pt_to_mm
 
 GENERIC = Style(
     font_family="Times",
@@ -19,7 +19,26 @@ GENERIC = Style(
     paragraph_margin_after=pt(4),
 )
 
+MODERN = Style(
+    font_family="Helvetica",
+    base_font_size=pt(11),
+    header_font_sizes={
+        1: pt(28),
+        2: pt(22),
+        3: pt(16),
+        4: pt(13),
+        5: pt(11),
+        6: pt(10),
+    },
+    header_line_height_factor=1.3,
+    paragraph_line_height_factor=1.6,
+    header_margin_after=pt(12),
+    paragraph_margin_after=pt(8),
+)
+
 STYLES = {
     "generic": GENERIC,
     "g": GENERIC,
+    "modern": MODERN,
+    "m": MODERN,
 }
